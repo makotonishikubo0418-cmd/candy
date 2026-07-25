84090443aa3a1d5c020a5334aa4d69c95413be71
makotonishikubo0418-cmd <224914155+makotonishikubo0418-cmd@users.noreply.github.com>
2026-07-26 08:48:37 +0900

    Fix noindex top preview tile layout

--- HP/preview/index.php
+++ HP/preview/index.php
@@ -19,6 +19,72 @@ if ($originalScriptFilename === null) {
 	$_SERVER['SCRIPT_FILENAME'] = $originalScriptFilename;
 }
 
+$tileScriptPath = '/home/firststar/public_html/group/candy/js/candyTile.js';
+$tileScript = file_get_contents($tileScriptPath);
+$tileGuard = 'if(!el || typeof gBox === "undefined"){ return false; }';
+$tileGuardFixed = 'if(!el || typeof gBox === "undefined" || typeof tmplate === "undefined"){ return false; }';
+$tileScheduleCall = 'scheduleTileInit();';
+
+if (
+	$tileScript === false
+	|| substr_count($tileScript, $tileGuard) !== 1
+	|| substr_count($tileScript, $tileScheduleCall) !== 1
+	|| stripos($tileScript, '</script') !== false
+) {
+	header('HTTP/1.1 500 Internal Server Error');
+	print 'Preview tile script preparation failed.';
+	exit;
+}
+
+$tileScript = str_replace($tileGuard, $tileGuardFixed, $tileScript);
+$tileScript = str_replace($tileScheduleCall, '', $tileScript);
+$tileScript = rtrim($tileScript) . "\n\n" . $tileScheduleCall . "\n";
+
+$tileScriptPattern = '#<script\s+defer\s+type="text/javascript"\s+src="\./js/candyTile\.js(?:\?v=[a-f0-9]+)?"></script>#';
+if (preg_match_all($tileScriptPattern, $html) !== 1) {
+	header('HTTP/1.1 500 Internal Server Error');
+	print 'Preview tile script replacement failed.';
+	exit;
+}
+$html = preg_replace_callback(
+	$tileScriptPattern,
+	function () use ($tileScript) {
+		return "<script type=\"text/javascript\">\n" . $tileScript . "</script>";
+	},
+	$html
+);
+
+$tileTemplateReplacements = array(
+	'<div class="photo"><img  src="imgHtml/dot.gif" alt="____photo____"></div>' =>
+		"<!--photo-->\n\t<div class=\"photo\"><img  src=\"imgHtml/dot.gif\" alt=\"____photo____\"></div>\n\t<!--photoEnd-->",
+	'<div class="movie"> <img  src="imgHtml/dot.gif"  alt="____photo____">' =>
+		"<!--video-->\n\t<div class=\"movie\"> <img  src=\"imgHtml/dot.gif\"  alt=\"____photo____\">",
+	'<div class="photo"><img src="imgHtml/dot.gif" alt="____photo____"></div>' =>
+		"<!--photo-->\n\t<div class=\"photo\"><img src=\"imgHtml/dot.gif\" alt=\"____photo____\"></div>\n\t<!--photoEnd-->",
+	'<div class="movie"> <img src="imgHtml/dot.gif" alt="____photo____">' =>
+		"<!--video-->\n\t<div class=\"movie\"> <img src=\"imgHtml/dot.gif\" alt=\"____photo____\">"
+);
+foreach ($tileTemplateReplacements as $search => $replacement) {
+	if (substr_count($html, $search) !== 1) {
+		header('HTTP/1.1 500 Internal Server Error');
+		print 'Preview tile template preparation failed.';
+		exit;
+	}
+	$html = str_replace($search, $replacement, $html);
+}
+
+$tileVideoEnd = "\t</div>\n\t<dl class=\"data vCenter\">";
+if (substr_count($html, $tileVideoEnd) !== 2) {
+	header('HTTP/1.1 500 Internal Server Error');
+	print 'Preview tile video marker preparation failed.';
+	exit;
+}
+$html = str_replace(
+	$tileVideoEnd,
+	"\t</div>\n\t<!--videoEnd-->\n\t<dl class=\"data vCenter\">",
+	$html
+);
+
 $robotsMeta = '<meta name="robots" content="index">';
 $headTag = '<head>';
 if (substr_count($html, $robotsMeta) !== 1 || substr_count($html, $headTag) !== 1) {
